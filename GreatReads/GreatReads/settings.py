2680fa9e6345c07fef0465c2eb25feee978f24a9
upobir <red.abiir@gmail.com>
2022-06-21 23:33:36 +0600

    setup heroku

--- GreatReads/GreatReads/settings.py
+++ GreatReads/GreatReads/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yh1(0gk2tj5f6v2)-rd=zkye5liz_ynuydi_ctkvms%(@kg%8r'
+# SECRET_KEY = 'django-insecure-yh1(0gk2tj5f6v2)-rd=zkye5liz_ynuydi_ctkvms%(@kg%8r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_on_heroku
+django_on_heroku.settings(locals())
