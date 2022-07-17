eca4bd7da7d01a67acc40103efcf2de462050042
B-DeshiDev <ffalsoy@gmail.com>
2022-07-17 12:48:26 +0600

    merge fixes

--- api/urls.py
+++ api/urls.py
@@ -22,8 +22,11 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('test', key_test, name="book"),
-    # path('login/', login)
+    path('book/<int:pk>/', get_book_info, name="book"),
+    path('books/', get_all_books, name="all_books"),
+    path('author/<int:pk>/', get_author_info, name="author"),
+    path('publisher/<int:pk>/', get_publisher_info, name="publisher"),
+    path('review/<int:pk>/', get_review_info, name="review"),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
