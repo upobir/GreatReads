e737751beca40dc5d5986fbcf999fd9757d94b18
B-DeshiDev <ffalsoy@gmail.com>
2022-07-23 14:18:58 +0600

    temp save

--- src/components/BrowseBooks.js
+++ src/components/BrowseBooks.js
@@ -5,7 +5,12 @@ import { BookSearchPreview } from './BookSearchPreview';
 import { Route, Routes } from 'react-router-dom';
 import { BrowseGenre } from './BrowseGenre';
 import {Spinner} from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
+import { useNavigate } from 'react-router-dom';
 const AllBooks=({books})=> {
+  const navigate = useNavigate()
+
   if(books.length <= 0){
     return <Container>
       <Spinner animation="border" variant="primary" />
@@ -40,17 +45,55 @@ export const BrowseBooks = () => {
     useEffect(() => {
       getBooks()
     }, [])
-  
+    const handleTabChange = (eventKey, e) => {
+      console.log('e', e)
+      navigate(eventKey); 
+    };
     // if(books.length <= 0)
       // return "loading..."
   
     return (
       <Container fluid>
+        <Col sm={3}>
+          <Nav variant="pills" className="flex-column" onSelect={handleTabChange}>
+            <Nav.Item>
+              <Nav.Link eventKey="all" href="#" className="browse-genre__tab-header">
+                All
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="byGenre" href="#" className="browse-genre__tab-header">
+                By Genre
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="byFollowedAuthors" href="#" className="browse-genre__tab-header">
+                By Followed Authors
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="newReleases" href="#" className="browse-genre__tab-header">
+                New Releases
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="newlyRated" href="#" className="browse-genre__tab-header">
+                Newly Rated
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col xs={5}>
           <Routes>
-              <Route path='/genre/:genreID/*' element={<BrowseGenre />} />
-              <Route path='/all/' element={<AllBooks books={books} />} />
+            <Route path='/genre/:genreID/*' element={<BrowseGenre />} />
+            <Route path='/all/' element={<AllBooks books={books} />} />
+            {/* <Route path="/byGenre" element={<SimilarBooksView similarBooks={_similar_books} />} /> */}
+            {/* <Route path="/byFollowedAuthors" element={<BookReview bookID={id} />}></Route> */}
+            {/* <Route path="/newReleases" element={<BookReviews book={book} />} /> */}
+            {/* <Route path="/newlyRated" element={<BookReviews bookID={id} />} /> */}
+            {/* <Route path="" element={<BookReviews book={book} />} /> */}
           </Routes>
-
+        </Col>
 
       </Container>
     )
--- src/components/BrowseGenre.js
+++ src/components/BrowseGenre.js
@@ -5,8 +5,15 @@ import { useParams } from 'react-router-dom'
 import { BookGallery } from './BookGallery';
 import {GenreDropDown} from './GenreDropDown'; 
 import { bookBrowseEndpoint } from '../endpoints';
+
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
+
 export const BrowseGenre = () => {
     const {genreID} = useParams();
+    const navigate = useNavigate()
+
     const [books, setBooks] = useState([])  
     const [genre, setGenre]  = useState({
         "id": 1,
@@ -26,22 +33,53 @@ export const BrowseGenre = () => {
         getNewBooksInGenre()
     }, [])
 
+
     return (
         <Container>
             {genre && 
-                <Stack gap={1}>
-                    <Stack direction="horizontal" gap={1}>
-                        <GenreDropDown selectedID ={genreID}/>
-                        <Stack>
-                            <h1>{genre.followerCount}</h1>
-                            <span>Following</span>
+                <>
+                <Tab.Container defaultActiveKey="byGenre">
+                    <Row>
+
+                        <Col sm={9}>
+                            {/* <Tab.Content>
+                                <Tab.Pane eventKey="byGenre">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dignissimos aspernatur dicta eveniet doloribus, in assumenda itaque dolorum? Maxime, totam. Possimus deleniti tenetur quam doloremque totam cumque voluptates dolorem ratione.
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="byFollowedAuthors">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae totam quo, fuga facere iste ex inventore voluptas id ipsam omnis distinctio adipisci dolore ducimus aut, explicabo nam vel ea tenetur.
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="newReleases">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum facere quos a eos, nihil voluptatem? Debitis voluptatum, corporis nihil saepe officiis amet repellendus ea, accusamus quis sapiente ipsam, labore praesentium!
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="newlyRated">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos iusto, nemo saepe beatae quia aspernatur hic consequatur vero explicabo provident similique praesentium! Accusamus nobis nulla, ratione expedita labore corrupti officia.
+                                </Tab.Pane>
+                            </Tab.Content> */}
+                            <Routes>
+                                <Route path="/byGenre" element={<SimilarBooksView similarBooks={_similar_books} />} />
+                                <Route path="/byFollowedAuthors" element={<BookReview bookID={id} />}></Route>
+                                <Route path="/newReleases" element={<BookReviews book={book} />} />
+                                <Route path="/newlyRated" element={<BookReviews bookID={id} />} />
+                                <Route path="" element={<BookReviews book={book} />} />
+                            </Routes>
+                        </Col>
+                    </Row>
+                </Tab.Container>
+                    <Stack gap={1}>
+                        <Stack direction="horizontal" gap={1}>
+                            <GenreDropDown selectedID ={genreID}/>
+                            <Stack>
+                                <h1>{genre.followerCount}</h1>
+                                <span>Following</span>
+                            </Stack>
+                            <Button variant="primary"> Follow </Button>
                         </Stack>
-                        <Button variant="primary"> Follow </Button>
+                        <div><p>{genre.description}</p></div>
+                        <h3 className='primary-text'>New releases Tagged "{genre.tag}":</h3>
+                        <BookGallery books={books}></BookGallery>
                     </Stack>
-                    <div><p>{genre.description}</p></div>
-                    <h3 className='primary-text'>New releases Tagged "{genre.tag}":</h3>
-                    <BookGallery books={books}></BookGallery>
-                </Stack>
+                </>
             }
         </Container>
     )
