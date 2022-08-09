ba35dcef838b2a339c98ce6c77f54b492ecdb748
B-DeshiDev <ffalsoy@gmail.com>
2022-08-09 16:29:29 +0600

    feed ui visuals fixed

--- src/components/BookBlurb.js
+++ src/components/BookBlurb.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import BookCapsule from './BookCapsule';
 import BookAuthorsBlock from './BookAuthorsBlock';
-import { Container, Stack } from 'react-bootstrap';
+import { Col,Row, Container, Stack } from 'react-bootstrap';
 
 
 export const BookBlurb = ({ book, setBook }) => {
 
-  return <Stack direction='horizontal' className='book-blurb'>
-    <BookCapsule book={book} setBook={setBook} id={book.id} />
-    <Container>
-      <Stack gap={1}>
+  return <Row className='book-blurb'>
+    <Col xs ={3}>
+      <BookCapsule book={book} setBook={setBook} id={book.id} />
+    </Col>
+    <Col xs={9} className='book-blurb__details'>
+      <Stack gap={1} fluid >
         <h1 className='primary-text'>{book?.title}</h1>
         <BookAuthorsBlock book={book} />
         <p className='medium-text'>{book?.description}</p>
       </Stack>
-    </Container>
-  </Stack>;
+    </Col>
+  </Row>;
 };
